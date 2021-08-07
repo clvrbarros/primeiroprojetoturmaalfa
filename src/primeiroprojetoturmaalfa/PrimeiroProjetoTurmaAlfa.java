dc45ad361996ed575ac09fea37a89241ca9bb6af
cleverson-barros <cleverson.barros@movile.com>
2021-08-07 12:12:32 -0300

    adicionando restante

---
+++ src/primeiroprojetoturmaalfa/PrimeiroProjetoTurmaAlfa.java
@@ -0,0 +1,22 @@
+package primeiroprojetoturmaalfa;
+
+public class PrimeiroProjetoTurmaAlfa {
+
+    public static void main(String[] args) {
+        // Operacoes matematicas
+        
+        //Declarando variáveis
+        int numeroA;
+        int numeroB;
+        int resultado;
+        
+        //Atribuindo valor as variaveis
+        numeroA = 8;
+        numeroB = 2;
+        
+        //Divisão
+        resultado = numeroA / numeroB;
+        System.out.println("Divisão: " + resultado);
+    }
+    
+}
